8378ce98b8d93c437d70a4dffe1c9f063e283a5c
PawelD <p.dmochewicz@gmail.com>
2017-12-15 15:02:58 +0100

    removed console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 function getTriangleArea(a, h) {
-	if((a > 0) && (h > 0)) return console.log(a*h/2);
+	if((a > 0) && (h > 0)) return a*h/2;
 	else return("Nieprawid³owe dane");
 	
 }
 
 var triangle1Area1 = getTriangleArea(2, 35);
 var triangle1Area2 = getTriangleArea(3, 4);
-var triangle1Area3 = getTriangleArea(10, 13);
+var triangle1Area3 = getTriangleArea(10, 13)
+
+console.log(triangle1Area1);
+console.log(triangle1Area2);
+console.log(triangle1Area3);
